4382c7c694d12f3847cdb6f2fa92098ef60319c0
parth060705 <gharatparth513@gmail.com>
2025-06-21 09:14:48 +0530

    small

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import { AuthProvider } from './hooks/UseAuth.jsx'; // Make sure the path is correct
+import { AuthProvider } from './hooks/useAuth.jsx'; // Make sure the path is correct
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

689cc07e6de60141629301726f21263efadf189e
parth060705 <parth.gharat@openlanesolutions.com>
2025-06-26 10:16:20 +0530

    correction 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import { AuthProvider } from './context/useAuth.jsx';
+// import { AuthProvider } from './context/useAuth.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+    {/* <AuthProvider> */}
       <App />
-    </AuthProvider>
+    {/* </AuthProvider> */}
   </StrictMode>
 );
